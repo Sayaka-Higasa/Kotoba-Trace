4da8911fc9046bb3db8e9ce0fb0413cc46a34c76
Sayaka-Higasa <A3kys98.4100fr@gmail.com>
2026-03-07 17:10:43 +0900

    words/views.pyに記録用viewを追加

--- words/views.py
+++ words/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def word_list(request):
     return render(request, "words/list.html")
+
+def word_record(request):
+    return render(request, "words/record.html")
