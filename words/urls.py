d43803065cb937b4eebed733d67b894c70d46729
Sayaka-Higasa <A3kys98.4100fr@gmail.com>
2026-03-07 17:06:30 +0900

    words/urls.pyにrecordのurlpatternsを追加

--- words/urls.py
+++ words/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = "words"
 urlpatterns = [
-    path("",views.word_list, name= "word_list") 
+    path("",views.word_list, name= "word_list") ,
+    path("record",views.word_record,name="word_record"),
 ]
