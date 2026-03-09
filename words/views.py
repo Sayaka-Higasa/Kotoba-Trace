3c9fc037811a2f34ad8e3c8f0d3823a561873b83
Sayaka-Higasa <A3kys98.4100fr@gmail.com>
2026-03-09 09:38:05 +0900

    record画面のPOST送信時の処理を追加

--- words/views.py
+++ words/views.py
@@ -6,3 +6,7 @@ def word_list(request):
 
 def word_record(request):
     return render(request, "words/record.html")
+
+def word_record(request):
+    if request.method == "POST":
+        return render(request, "words/record.html", {"message": "保存が完了しました！"})
