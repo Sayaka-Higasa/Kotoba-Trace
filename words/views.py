8ecf7158d1fc8aef057f2a2ee5555b7d89d4c9b2
Sayaka-Higasa <A3kys98.4100fr@gmail.com>
2026-03-09 15:48:21 +0900

    word_recordページのviewにデータ保存処理を追加

--- words/views.py
+++ words/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from. models import Word
 
 # Create your views here.
 def word_list(request):
@@ -9,5 +10,15 @@ def word_record(request):
 
 def word_record(request):
     if request.method == "POST":
+        Word.objects.create(
+            user = request.user,
+            content = request.POST.get("content"),
+            source_type = request.POST.get("source_type"),
+            source_title = request.POST.get("source_title"),
+            source_creator = request.POST.get("source_creator"),
+            memo = request.POST.get("memo"),
+            is_public = request.POST.get("is_public") =="on"
+        )
+
         return render(request, "words/record.html", {"message": "保存が完了しました！"})
     return render(request , "words/record.html")
