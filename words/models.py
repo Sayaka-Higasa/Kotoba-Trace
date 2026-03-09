3565d817005fa811c106d25ae5270236fd52ff3e
Sayaka-Higasa <A3kys98.4100fr@gmail.com>
2026-03-09 11:40:02 +0900

    Wordモデルを作成

--- words/models.py
+++ words/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Word(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    content = models.CharField(max_length= 300) #ことば
+    source_type = models.CharField(max_length= 20) #出典
+    source_title = models.CharField(max_length= 100 , blank = True) #タイトル
+    source_creator = models.CharField(max_length= 100 , blank = True) #著者、アーティスト
+    memo = models.CharField(max_length= 500 , blank = True) #メモ
+    is_public = models.BooleanField(default= True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
