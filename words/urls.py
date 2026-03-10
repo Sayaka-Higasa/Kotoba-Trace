e554649642a846c60a9e578475467db0de811815
Sayaka-Higasa <A3kys98.4100fr@gmail.com>
2026-03-10 22:21:25 +0900

    言葉詳細のpathを追加

--- words/urls.py
+++ words/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 app_name = "words"
 urlpatterns = [
-    path("words",views.word_list, name= "word_list") , #一覧ページ
+    path("",views.word_list, name= "word_list") , #一覧ページ
     path("words/record",views.word_record,name="word_record"), #記録ページ
-    path("words/<int:word_id>/", views.word_detail,name="word_detail"), #詳細
+    path("<int:word_id>/", views.word_detail,name="word_detail"), #詳細
 ]
+
