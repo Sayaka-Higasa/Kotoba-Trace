1f6ee0201c491c7a1d9ab274d652a2942ce80093
Sayaka-Higasa <A3kys98.4100fr@gmail.com>
2026-03-12 11:50:15 +0900

    wordsアプリのurls.pyに言葉編集のpath追加

--- words/urls.py
+++ words/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("",views.word_list, name= "word_list") , #一覧ページ
     path("words/record",views.word_record,name="word_record"), #記録ページ
     path("<int:word_id>/", views.word_detail,name="word_detail"), #詳細
-]
+    path("edit<int:word_id>/" , views.word_edit, name="word_edit"), #編集
+    ]
 
