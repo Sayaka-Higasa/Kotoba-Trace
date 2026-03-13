33b9f7829c716ff2f49d1f361c919070a5448171
Sayaka-Higasa <A3kys98.4100fr@gmail.com>
2026-03-13 15:26:35 +0900

    accountsアプリのurls.pyのloginパスをメールログイン対応に更新

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path 
 from django . contrib.auth import views as auth_views
+from .views import EmailLoginForm
 
 app_name = "accounts"
 urlpatterns = [
     path("login/", auth_views.LoginView.as_view(template_name = "accounts/login.html"), name= "login") , #ログインページ
     path("logout/" , auth_views.LogoutView.as_view(), name = "logout"),
+    path(
+        "login/",
+        auth_views.LoginView.as_view(
+            template_name="accounts/login.html",
+            authentication_form=EmailLoginForm
+        ),
+        name="login"
+    ),
     ]
