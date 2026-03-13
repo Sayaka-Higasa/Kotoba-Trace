5619934265d2829364bb4192f3bf6639afd68264
Sayaka-Higasa <A3kys98.4100fr@gmail.com>
2026-03-13 10:12:51 +0900

    WordForm 修正、タグを管理からはずす

--- words/forms.py
+++ words/forms.py
@@ -5,4 +5,4 @@ class  WordForm(forms.ModelForm):
     class Meta:
         model = Word
         fields = ["content", "source_type", "source_title", "source_creator",
-        "memo", "is_public", "tags"]
+        "memo", "is_public" ]
