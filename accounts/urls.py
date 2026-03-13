c4eb6ca1013f08689726415f183db3435437f1b5
Sayaka-Higasa <A3kys98.4100fr@gmail.com>
2026-03-13 14:47:29 +0900

    accountsアプリのurlsにログイン、ログアウトのurlパターン追加

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path 
-from . import views
+from django . contrib.auth import views as auth_views
 
 app_name = "accounts"
 urlpatterns = [
-    path("login/", views.login_view, name= "login") , #ログインページ
-
+    path("login/", auth_views.LoginView.as_view(template_name = "accounts/login.html"), name= "login") , #ログインページ
+    path("logout/" , auth_views.LogoutView.as_view(), name = "logout"),
     ]
