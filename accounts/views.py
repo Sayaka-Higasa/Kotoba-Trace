bcd45eaf5b2212dcb6b5b1c860f4b83260423031
Sayaka-Higasa <A3kys98.4100fr@gmail.com>
2026-03-13 15:25:10 +0900

    accountsアプリのviewにメアドでログインできるフォームを追加

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.contrib. auth. forms import AuthenticationForm
+from django.contrib. auth import authenticate
+from django. contrib. auth. models import User
+from django import forms
 
 # Create your views here.
+class EmailLoginForm(AuthenticationForm):
+    username = forms.EmailField(label="Email")
+
+    def clean(self):
+        email = self. cleaned_data.get("username")
+        password = self. cleaned_data.get( "password")
+        if email and password :
+            try:
+                user_obj = User.objects.get("email=email")
+            except User.DoesNotExist:
+                raise forms.ValidationError("メールアドレスが存在しません")
+            self.user_cache = authenticate(username=user_obj.username, password = password)
+            if self.user_cache is None:
+                raise forms.ValidationError("パスワードが間違っています")
+        return self.cleaned_data
