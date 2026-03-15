4630fcfd523548d7f5d40f9e995a55230445e44f
Sayaka-Higasa <A3kys98.4100fr@gmail.com>
2026-03-15 17:39:33 +0900

    accountsアプリのurlsにfrom . import viewsを追加

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path 
 from django.contrib.auth import views as auth_views
 from .views import EmailLoginForm, signup_view
+from . import views
 
 app_name = "accounts"
 urlpatterns = [
-   
     path(
         "login/",
         auth_views.LoginView.as_view(
@@ -14,5 +14,8 @@ urlpatterns = [
         name="login"
     ),
     path("logout/" , auth_views.LogoutView.as_view(), name = "logout"),
-    path("signup/", signup_view, name="signup") #新規登録 
-    ]
+    path("signup/", signup_view, name="signup") ,#新規登録 
+    #パスワードリセット関連
+    path("password_reset/" , views.password_reset_request , name = "password_reset"),
+    path("reset/<uuid:token>/", views.password_reset_confirm, name= "password_reset_confirm"),
+]
