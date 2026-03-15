8bfe934abd0cd5403e26aa648b802e36b4896681
Sayaka-Higasa <A3kys98.4100fr@gmail.com>
2026-03-15 17:23:39 +0900

    accountsアプリのmodelを設定

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+import uuid
+from django.db import models
+from django.contrib.auth.models import User
+
+class PasswordReset(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    password_token = models.UUIDField(
+        default=uuid.uuid4,
+        editable=False,
+        unique=True
+    )
+
+    password_expiry = models.DateTimeField()
+
+    is_used = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    updated_at = models.DateTimeField(auto_now=True)
