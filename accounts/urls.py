bfd8062a26ec93b8ae414beabfd41d41c4a3e399
Sayaka-Higasa <A3kys98.4100fr@gmail.com>
2026-03-17 09:15:08 +0900

    accountsアプリのurlsに設定画面用pathを追加

--- accounts/urls.py
+++ accounts/urls.py
@@ -18,4 +18,11 @@ urlpatterns = [
     #パスワードリセット関連
     path("password_reset/" , views.password_reset_request , name = "password_reset"),
     path("reset/<uuid:token>/", views.password_reset_confirm, name= "password_reset_confirm"),
+
+    #設定画面
+    path("settings/", views.settings_view,name="settings"),
+    path ("email-change/",views.email_change, name="email_change"),
+    path("password_change/", auth_views.PasswordChangeView.as_view(
+        template_name="accounts/password_change.html"
+    ),name="password_change"),
 ]
