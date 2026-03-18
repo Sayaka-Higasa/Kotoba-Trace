895890b0216e95747dd6678c87b10a2189cb9b54
Sayaka-Higasa <A3kys98.4100fr@gmail.com>
2026-03-18 14:43:55 +0900

    words/urls.pyに削除用pathを追加

--- words/urls.py
+++ words/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("words/record",views.word_record,name="word_record"), #記録ページ
     path("<int:word_id>/", views.word_detail,name="word_detail"), #詳細
     path("edit<int:word_id>/" , views.word_edit, name="word_edit"), #編集
+    path("edit<int:word_id>/delete/", views.word_delete, name="word_delete"),
     ]
 
