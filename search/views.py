48f019ff77960e684ad6a69bd0a1e4ba7174c89a
Sayaka-Higasa <A3kys98.4100fr@gmail.com>
2026-03-18 10:37:17 +0900

    searchアプリのviewsの検索ページについての記載修正

--- search/views.py
+++ search/views.py
@@ -8,7 +8,7 @@ def index(request):
     # 公開されている言葉を取得
     candidates = Word.objects.filter(is_public=True).order_by('-id')
 
-    # ログイン中なら自分の投稿は除外
+    # 自分の投稿は除外
     if request.user.is_authenticated:
         candidates = candidates.exclude(user=request.user)
 
@@ -26,13 +26,20 @@ def results(request):
     words = Word.objects.none()
 
     if query:
-        if '#' in query:
+        if query.startswith('#'):
             # タグ検索
-            words = Word.objects.filter(tags__icontains=query)
+            tag_keyword = query.lstrip("#")
+            words = Word.objects.filter(
+                tags__name__icontains=tag_keyword
+            ).distinct()
         else:
             # キーワード検索（本文 OR 出典）
             words = Word.objects.filter(
-                Q(content__icontains=query) | Q(source_type__icontains=query)
-            )
+                Q(content__icontains=query) | 
+                Q(source_type__icontains=query) |
+                Q(source_title__icontains=query) |
+                Q(source_creator__icontains=query)
+            ).distinct()
+        words = words.order_by("-created_at")
 
     return render(request, 'search/results.html', {'words': words, 'query': query})
