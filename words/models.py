a31d5f4ba7e9f245084ad03cd295c71aef5db9e8
Sayaka-Higasa <A3kys98.4100fr@gmail.com>
2026-03-20 11:36:56 +0900

    words/models.pyのタグフィールドを修正

--- words/models.py
+++ words/models.py
@@ -1,24 +1,34 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-
+# Word モデル
 class Word(models.Model):
-    user = models.ForeignKey(User, on_delete = models.CASCADE)
-    content = models.CharField(max_length= 300) #ことば
-    source_type = models.CharField(max_length= 20) #出典
-    source_title = models.CharField(max_length= 100 , blank = True) #タイトル
-    source_creator = models.CharField(max_length= 100 , blank = True) #著者、アーティスト
-    memo = models.CharField(max_length= 500 , blank = True) #メモ
-    is_public = models.BooleanField(default= True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.CharField(max_length=300)
+    source_type = models.CharField(max_length=20)
+    source_title = models.CharField(max_length=100, blank=True)
+    source_creator = models.CharField(max_length=100, blank=True)
+    memo = models.CharField(max_length=500, blank=True)
+    is_public = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    tags = models.ManyToManyField("Tag" , blank = True)
+    tags = models.ManyToManyField("Tag", through="WordTag", blank=True)
 
-#タグテーブル
+# Tag モデル
 class Tag(models.Model):
-    name = models.CharField(max_length= 50) #タグ名
+    name = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
     def __str__(self):
         return self.name
+
+# 中間テーブル（WordTag）
+class WordTag(models.Model):
+    word = models.ForeignKey(Word, on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ("word", "tag")
