d4f3668d83f3b50eae77d3bd7e0641c2e4ab848a
Sayaka-Higasa <A3kys98.4100fr@gmail.com>
2026-03-20 22:44:47 +0900

    言葉編集画面のエラー文修正

--- words/forms.py
+++ words/forms.py
@@ -13,6 +13,23 @@ class  WordForm(forms.ModelForm):
         fields = ["content", "source_type", "source_title", "source_creator",
         "memo", "is_public" ]
 
+        labels = {
+            "content": "ことば",
+            "source_type": "出典",
+            "source_title": "作品名・曲名",
+            "source_creator": "著者・アーティスト",
+            "memo": "メモ",
+        }
+
+        error_messages = {
+            "source_type" : {
+                "required" : "出典を入力してください"
+            },
+            "content" : {
+                "required" : "ことばを入力してください"
+            },
+        }
+
     def clean_is_public(self):
         data = self.cleaned_data.get('is_public')
         return 1 if data else 0
