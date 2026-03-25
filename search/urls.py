37b323ac80da7f751bb196ebd685531f3ecac8e5
Sayaka-Higasa <A3kys98.4100fr@gmail.com>
2026-03-25 20:55:08 +0900

    search/urls.pyで検索画面のURL修正

--- search/urls.py
+++ search/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 app_name = "search"
 urlpatterns = [
-    path("",views.index, name = "index"),
+    path("search/",views.index, name = "index"),
     path('results/', views.results, name= 'results'),
 ]
