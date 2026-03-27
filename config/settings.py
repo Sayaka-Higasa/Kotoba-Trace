2aa9fc2791574c4aaa770dd6f05f8b9c3208d1f8
Sayaka-Higasa <A3kys98.4100fr@gmail.com>
2026-03-27 20:08:34 +0900

    settings.pyにstatic roof追加

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 from django.urls import reverse_lazy 
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,9 +132,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+
 
 
 LOGIN_URL = 'accounts:login'
